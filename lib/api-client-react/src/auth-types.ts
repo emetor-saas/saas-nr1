6c8ab86fa1bc5c47e54180bb637ed522cd5acf2f
Development-Emetor <cto@emetor.com.br>
2026-06-26 12:23:40 -0300

    chore: add lib/api-client-react/src/auth-types.ts

---
+++ lib/api-client-react/src/auth-types.ts
@@ -0,0 +1,8 @@
+export interface AuthUser {
+  id: string;
+  username?: string | null;
+  firstName?: string | null;
+  lastName?: string | null;
+  email?: string | null;
+  profileImageUrl?: string | null;
+}
