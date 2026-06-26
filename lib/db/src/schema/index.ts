8c80cfffeaf160d5b28c8de57a1d19736cb9c709
Development-Emetor <cto@emetor.com.br>
2026-06-26 12:24:57 -0300

    chore: add lib/db/src/schema/index.ts

---
+++ lib/db/src/schema/index.ts
@@ -0,0 +1,7 @@
+export * from "./tenants";
+export * from "./organizations";
+export * from "./nr1";
+export * from "./recruitment";
+export * from "./conversations";
+export * from "./messages";
+export * from "./auth";
